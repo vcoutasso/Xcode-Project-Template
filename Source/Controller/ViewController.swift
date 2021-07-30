86341e613a5324d3e31f555f7fec1e2d52aa69bc
Vinícius Couto <vcoutasso@gmail.com>
2021-07-30 20:14:46 -0300

    Add UIKit template

---
+++ Source/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PROJECT_NAME
+//
+//  Created by Vinícius Couto on 30/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
