7c4b5b9a4c8c7167453a9b69bbcdb63d4148908a
Vinícius Couto Tasso <vcoutasso@gmail.com>
2021-10-13 00:34:52 -0300

    Add SwiftUI template

---
+++ App/SwiftUI/Sources/App.swift
@@ -0,0 +1,17 @@
+//
+//  PROJECT_NAMEApp.swift
+//  PROJECT_NAME
+//
+//  Created by Vinícius Couto on 30/07/21.
+//
+
+import SwiftUI
+
+@main
+struct PROJECT_NAMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/SwiftUI/Sources/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PROJECT_NAME
+//
+//  Created by Vinícius Couto on 30/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
